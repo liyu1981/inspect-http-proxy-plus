428046e3d6ba432c6176a8aa26b1602daed14fb4
Yu Li <liyu1981@gmail.com>
2026-02-19 11:54:20 +1100

    chore: restrict npm package contents using 'files' field

--- scripts/sync-version.js
+++ scripts/sync-version.js
@@ -8,12 +8,12 @@ function syncVersion() {
   try {
     const constantsContent = fs.readFileSync(constantsPath, 'utf8');
     const versionMatch = constantsContent.match(/const Version = "([^"]+)"/);
-    
+
     if (!versionMatch) {
       console.error('Could not find Version constant in pkg/core/constants.go');
       process.exit(1);
     }
-    
+
     const version = versionMatch[1];
     if (version === 'dev') {
       console.log('Backend version is "dev", skipping sync.');
@@ -24,8 +24,7 @@ function syncVersion() {
     if (pkg.version !== version) {
       console.log(`Syncing package.json version: ${pkg.version} -> ${version}`);
       pkg.version = version;
-      fs.writeFileSync(packageJsonPath, JSON.stringify(pkg, null, 2) + '
-');
+      fs.writeFileSync(packageJsonPath, JSON.stringify(pkg, null, 2) + '\n');
     } else {
       console.log(`Versions already match: ${version}`);
     }
